1c641fa5272d170eeb86c1e5a8ff6264f6a9ceac
mobarokhossan <mobarok.hossan.bd@gmail.com>
2026-06-02 02:33:42 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Minimal JS only (performance optimized)
+
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.addEventListener("click",e=>{
+    const t=document.querySelector(a.getAttribute("href"));
+    if(t){e.preventDefault();t.scrollIntoView({behavior:"smooth"});}
+  });
+});
